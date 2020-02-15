c060a1d54019651cadb1f0a9f9d3dc5c868bc4e1
Anton <anton_real_madrid@icloud.com>
2020-02-15 18:37:06 +0100

    added asyncHandler

--- src/controllers/courses.ts
+++ src/controllers/courses.ts
@@ -1,79 +1,60 @@
 import express, { Request, Response, NextFunction } from 'express';
 
 import Course from '../models/Course';
+import { asyncHandler } from '../middleware/async';
 import { YoutubeApiHelper } from '../helpers/youtube';
 import { ErrorResponse } from '../helpers/errorResponse';
 
 // @desc   Get all courses
 // @route  GET /api/v1/courses
 // @access Public
-export const getCourses = async (req: Request, res: Response, next: NextFunction) => {
+export const getCourses = asyncHandler( async (req: Request, res: Response, next: NextFunction) => {
   await YoutubeApiHelper.getCourses();
-  try {
     const courses = await Course.find();
     res.status(200).json({ success: true, count: courses.length, data: courses});
-  } catch (err) {
-    next(err);
-  } 
-}
+});
 
 // @desc   Get single courses
 // @route  GET /api/v1/courses/:id
 // @access Public
-export const getCourse = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+export const getCourse = asyncHandler( async (req: Request, res: Response, next: NextFunction) => {
     const course = await Course.findById(req.params.id);
 
-    if (!course) return next(new ErrorResponse(`Resource not found with that id`, 404));
+    if (!course) return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
 
     res.status(200).json({ success: true, data: course});
-  } catch (err) {
-    next(err);
-  }
-}
+});
 
 // @desc   Create new course
 // @route  POST /api/v1/courses
 // @access Private
-export const createCourse = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+export const createCourse = asyncHandler( async (req: Request, res: Response, next: NextFunction) => {
     const createdCourse = await Course.create(req.body);
     res.status(201).json({ success: true, data: createdCourse});
-  } catch (err) {
-    next(err);
-  }
-}
+});
 
 // @desc   Update course
 // @route  PUT /api/v1/courses/:id
 // @access Private
-export const updateCourse = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+export const updateCourse = asyncHandler( async (req: Request, res: Response, next: NextFunction) => {
     const course = await Course.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true
     });
   
-    if(!course) return next(new ErrorResponse(`Resource not found with that id`, 404));
+    if(!course) return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
 
     res.status(200).json({ success: true, data: course});
-  } catch (err) {
-    next(err);
-  }
-}
+});
 
 // @desc   Delete course
 // @route  DELETE /api/v1/courses/:id
 // @access Private
-export const deleteCourse = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+export const deleteCourse = asyncHandler( async (req: Request, res: Response, next: NextFunction) => {
     const course = await Course.findByIdAndDelete(req.params.id);
   
-    if(!course) return next(new ErrorResponse(`Resource not found with that id`, 404));
+    if(!course) return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
 
     res.status(200).json({ success: true, data: {}});
-  } catch (err) {
-    next(err);
-  }
-}
+});
 
--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import mongoose, { CastError } from 'mongoose';
 import colors from 'colors';
 
 import { ErrorResponse } from '../helpers/errorResponse';
@@ -8,11 +9,20 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
 
   // Dev log
   console.log(colors.red(err.stack!));
-
-  // Mongoose bad ObjectId
+  console.log(err.message);
+  
   if(err.name === 'CastError') {
-    const message = `Resource not found with that id`
+    // Mongoose bad ObjectId
+    const castErr = err as CastError;
+    const message = `Resource not found with id of ${castErr.value}`
     error = new ErrorResponse(message, 404);
+  } else if(err.message.includes("E11000 duplicate key error collection")) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  } else if(err.name === 'ValidationError') {
+    const valErr = err as mongoose.Error.ValidationError;
+    const commaSeparatedErrorMsgs = Object.values(valErr.errors).map(er => er.message).join(',');
+    error = new ErrorResponse(commaSeparatedErrorMsgs, 400);
   } else {
     if(err instanceof ErrorResponse) {
       error = new ErrorResponse(err.message, err.statusCode);
